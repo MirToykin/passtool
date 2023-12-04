07b57def4b6e7bf2336da7ad72336706c50bd019
Miroslav <mt@truefuture.io>
2023-12-04 11:19:42 +0400

    handled empty input

--- internal/lib/cli/cli.go
+++ internal/lib/cli/cli.go
@@ -10,18 +10,33 @@ import (
 
 func GetUserInput(prompt string) string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(prompt)
-	input, _ := reader.ReadString('\n')
-	return strings.TrimSpace(input)
+	for {
+		fmt.Print(prompt)
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+
+		if input == "" {
+			fmt.Println("value can't be empty")
+		} else {
+			return input
+		}
+	}
 }
 
 func GetSensitiveUserInput(prompt string) (string, error) {
-	fmt.Print(prompt)
-	bytePassword, err := term.ReadPassword(int(os.Stdin.Fd()))
-	if err != nil {
-		return "", err
-	}
-	fmt.Println() // Print a newline because ReadPassword does not capture the enter key
+	for {
+		fmt.Print(prompt)
+		bytePassword, err := term.ReadPassword(int(os.Stdin.Fd()))
+		if err != nil {
+			return "", err
+		}
 
-	return string(bytePassword), nil
+		fmt.Println() // Print a newline because ReadPassword does not capture the enter key
+
+		if len(bytePassword) == 0 {
+			fmt.Println("value can't be empty")
+		} else {
+			return string(bytePassword), nil
+		}
+	}
 }
